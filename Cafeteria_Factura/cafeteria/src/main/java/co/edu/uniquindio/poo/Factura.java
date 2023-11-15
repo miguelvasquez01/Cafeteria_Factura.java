8e6b8869904035652114c003708769ab07af9f47
Miguel Vasquez <miguela.vasquezg@uqvirtual.edu.co>
2023-11-14 23:53:44 -0500

    prueba dos interfaces

--- Cafeteria_Factura/cafeteria/src/main/java/co/edu/uniquindio/poo/Factura.java
+++ Cafeteria_Factura/cafeteria/src/main/java/co/edu/uniquindio/poo/Factura.java
@@ -117,7 +117,7 @@ public class Factura {
         if(cliente instanceof ClienteEstudiante) {
             ClienteEstudiante estudiante = (ClienteEstudiante)cliente;
 
-            double descuentoSemestre = estudiante.getSemestre() * 0.0124499;
+            double descuentoSemestre = estudiante.getSemestre() * 0.012777;
             descuento = precioTotal * descuentoSemestre;
         }
 
